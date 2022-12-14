6d71770e1d28e4340d617f14d2e6036445efcaac
isabellexvr <isabellexavier59@gmail.com>
2022-12-14 18:16:58 -0200

    fix: registration controller - passwd encryptation

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -1,16 +1,35 @@
 import { connectionDB } from "../database/db.js";
+import bcrypt from "bcrypt";
+import { v4 as uuidV4 } from "uuid";
 
 export async function registration(req, res) {
-    const {name, email, password} = req.body;
-    try{
-        await connectionDB.query("INSERT INTO users (name, email, password) VALUES ($1,$2,$3)", [name, email, password])
-        res.status(201).send("Usuário registrado com sucesso!")
-    }catch (err) {
+  const { name, email, password } = req.body;
+  try {
+    const hidePassword = bcrypt.hashSync(password, 10);
+    await connectionDB.query(
+      "INSERT INTO users (name, email, password) VALUES ($1,$2,$3)",
+      [name, email, hidePassword]
+    );
+    res.status(201).send("Usuário registrado com sucesso!");
+  } catch (err) {
     res.status(500).send(err.message);
     console.log(err.message);
   }
 }
 
 export async function login(req, res) {
-    
+  const { email, password } = req.body;
+  const token = uuidV4();
+  try {
+    const emailExists = await connectionDB.query(
+      "SELECT * FROM users WHERE email=$1",
+      [email]
+    );
+    if (emailExists.rows.length < 1) {
+      return res.status(401).send("Esse e-mail não está cadastrado.");
+    }
+  } catch (err) {
+    res.status(500).send(err.message);
+    console.log(err.message);
+  }
 }
