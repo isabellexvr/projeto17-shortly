c43e16d2cdf583f1bfa21ee61c256d7d1d8794b3
isabellexvr <isabellexavier59@gmail.com>
2022-12-14 18:34:49 -0200

    feat: login controller verify auth

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -17,17 +17,41 @@ export async function registration(req, res) {
   }
 }
 
+//fazer middleware do login
+
 export async function login(req, res) {
   const { email, password } = req.body;
   const token = uuidV4();
   try {
-    const emailExists = await connectionDB.query(
+    const userExists = await connectionDB.query(
       "SELECT * FROM users WHERE email=$1",
       [email]
     );
-    if (emailExists.rows.length < 1) {
+    if (userExists.rows.length < 1) {
       return res.status(401).send("Esse e-mail não está cadastrado.");
     }
+    const sessionExists = await connectionDB.query(
+      "SELECT * FROM sessions WHERE userId=$1",
+      [userExists.rows[0].id]
+    );
+    if (sessionExists.rows.length > 0) {
+      return res
+        .status(400)
+        .send("Você já está logado, saia para poder logar novamente.");
+    }
+    const comparePasswords = bcrypt.compareSync(
+      password,
+      userExists.rows[0].password
+    );
+    if (comparePasswords) {
+      return res.status(401).send("Senha incorreta.");
+    }
+
+    await connectionDB.query("INSERT INTO sessions (token, userId)", [
+      token,
+      userExists.rows[0].id,
+    ]);
+    res.status(200).send("Login realizado com sucesso!")
   } catch (err) {
     res.status(500).send(err.message);
     console.log(err.message);
