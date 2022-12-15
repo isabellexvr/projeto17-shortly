c9ba1619014ddd13dddb9e761af61bb2c3fb2e8c
isabellexvr <isabellexavier59@gmail.com>
2022-12-15 15:03:57 -0200

    feat: urls routes

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes";
+import urlsRoutes from "./routes/urlsRoutes";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use(authRoutes);
+app.use(urlsRoutes)
 
 app.listen(4000, () => console.log("Server is running in port 4000"));
