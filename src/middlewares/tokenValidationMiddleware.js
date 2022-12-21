f57035085edaaf01de96ee9d07024c2a5b382b89
isabellexvr <isabellexavier59@gmail.com>
2022-12-21 00:04:58 -0300

    refactor: token validation middleware

--- src/middlewares/tokenValidationMiddleware.js
+++ src/middlewares/tokenValidationMiddleware.js
@@ -1,30 +1,31 @@
 import { connectionDB } from "../database/db.js";
 
 export async function tokenValidation(req, res, next) {
-  console.log("oi")
   const { authorization } = req.headers;
   const token = authorization?.replace("Bearer ", "");
   if (!token) {
     return res.status(401).send("Nenhum token foi enviado.");
   }
   try {
-    const sessionExists = connectionDB.query(
+    const sessionExists = await connectionDB.query(
       `SELECT * FROM sessions WHERE token=$1`,
       [token]
     );
     if (sessionExists.rows.length < 1) {
       return res.status(401).send("Não existe sessão com essa token");
     }
-    const userExists = connectionDB.query(`SELECT * FROM users WHERE id=1$`, [
-      sessionExists.rows[0].userId,
-    ]);
+    const userExists = await connectionDB.query(
+      `SELECT * FROM users WHERE id=$1`,
+      [sessionExists.rows[0].userId]
+    );
     if (userExists.rows.length < 1) {
       return res.status(401).send("Esse usuário não existe.");
     }
+    res.locals.userId = sessionExists.rows[0].userId;
   } catch (err) {
     res.status(500).send(err.message);
     console.log(err.message);
   }
-  res.locals.userId = sessionExists.rows[0].userId
+
   next();
 }
