c54a74cc00d0f06648d3b39680e0957e8cf9a47a
isabellexvr <isabellexavier59@gmail.com>
2022-12-21 11:02:24 -0300

    fix: find all urls by id controller queries

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -3,22 +3,33 @@ import { connectionDB } from "../database/db.js";
 export default async function findAllUrlsById(req, res) {
   const userId = res.locals.userId;
   try {
-    const { rows } = await connectionDB.query(
+    const userInfo = await connectionDB.query(
       `
         SELECT 
             users.id, 
             users.name, 
-            COUNT(urls."visits") AS "visitCount", 
-            urls.* 
+            COUNT(urls."visits") AS "visitCount"
         FROM users 
         JOIN urls ON users.id=urls."userId"
         WHERE users.id=$1
-        GROUP BY urls.id, users.id
+        GROUP BY users.id
         ;
         `,
       [userId]
-    )
-    res.send(rows)
+    );
+    const userUrls = await connectionDB.query(
+      `SELECT u.id, 
+        u."shortUrl", 
+        u.url, 
+        u.visits AS "visitCount" 
+      FROM urls u 
+      WHERE "userId"=$1;`,
+      [userId]
+    );
+    res.send({
+      ...userInfo.rows[0],
+      shortenedUrls: userUrls.rows,
+    });
   } catch (err) {
     res.status(500).send(err.message);
     console.log(err.message);
