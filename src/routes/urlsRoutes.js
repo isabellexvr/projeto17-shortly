8712da90b957ba3f858f239b32d3d041d4b13d6f
isabellexvr <isabellexavier59@gmail.com>
2022-12-20 22:26:00 -0300

    fix: find url by id route

--- src/routes/urlsRoutes.js
+++ src/routes/urlsRoutes.js
@@ -1,14 +1,16 @@
 import { Router } from "express";
 import { tokenValidation } from "../middlewares/tokenValidationMiddleware.js";
 import urlModelValidation from "../middlewares/urlModelValidationMiddleware.js";
-import { shortUrl } from "../controllers/urlsControllers.js";
+import { shortUrl, findUrlById } from "../controllers/urlsControllers.js";
 
 const urlsRoutes = Router();
+
+urlsRoutes.get("/urls/:id", findUrlById)
 
 urlsRoutes.use(tokenValidation)
 
 urlsRoutes.post("/urls/shorten", urlModelValidation, shortUrl)
 
-urlsRoutes.get("/urls/:id")
+
 
 export default urlsRoutes;
