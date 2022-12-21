0580e8af7c016803e1c6e6a194145cc901221aeb
isabellexvr <isabellexavier59@gmail.com>
2022-12-20 22:42:13 -0300

    fix: redirect to url route

--- src/routes/urlsRoutes.js
+++ src/routes/urlsRoutes.js
@@ -1,17 +1,19 @@
 import { Router } from "express";
 import { tokenValidation } from "../middlewares/tokenValidationMiddleware.js";
 import urlModelValidation from "../middlewares/urlModelValidationMiddleware.js";
-import { shortUrl, findUrlById } from "../controllers/urlsControllers.js";
+import { shortUrl, findUrlById, redirectToUrl } from "../controllers/urlsControllers.js";
 
 const urlsRoutes = Router();
 
 urlsRoutes.get("/urls/:id", findUrlById)
 
-urlsRoutes.get("/urls/open/:shortUrl", )
+urlsRoutes.get("/urls/open/:shortUrl", redirectToUrl)
 
 urlsRoutes.use(tokenValidation)
 
 urlsRoutes.post("/urls/shorten", urlModelValidation, shortUrl)
+
+urlsRoutes.delete("/urls/:id", )
 
 
 
