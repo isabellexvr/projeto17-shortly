4c2f5bbb11ab62a2fafcd2941e0cab959ef2a585
isabellexvr <isabellexavier59@gmail.com>
2022-12-21 14:20:34 -0300

    feat: ranking controller

--- src/controllers/rankingController.js
+++ src/controllers/rankingController.js
@@ -1,5 +1,23 @@
 import { connectionDB } from "../database/db.js";
 
-export default async function findRanking(req,res){
-    
+export default async function findRanking(req, res) {
+  try {
+    const { rows } = await connectionDB.query(`
+        SELECT 
+            users.id, 
+            users.name, 
+            COUNT(urls."userId") AS "linksCount", 
+            COUNT(urls.visits) AS "visitCount"
+        FROM users
+        JOIN urls ON urls."userId"=users.id
+        GROUP BY users.id
+        ORDER BY "visitCount" DESC
+        LIMIT 10
+        ;
+        `);
+    res.status(200).send(rows);
+  } catch (err) {
+    res.status(500).send(err.message);
+    console.log(err.message);
+  }
 }
--- src/routes/rankingRoute.js
+++ src/routes/rankingRoute.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import findRanking from "../controllers/rankingController.js"
+import findRanking from "../controllers/rankingController.js";
 
 const rankingRoute = Router();
 
-rankingRoute.get("/ranking", findRanking)
+rankingRoute.get("/ranking", findRanking);
 
 //deixar sem o {} o import de ranking e de users, pq só importa 1 função
 
